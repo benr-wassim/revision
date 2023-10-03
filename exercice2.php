d76c61e7539c74c5b542ba745db2607d285d87fe
BEN_BOUBAKER WASSIM <benboubaker-wassim@lycee-jeanrostand.fr>
2023-10-03 16:20:48 +0200

    table de modification

--- exercice2.php
+++ exercice2.php
@@ -3,15 +3,10 @@
 for ($i = 1; $i <= 5; $i++) {
     // Boucle pour les colonnes de 1 à 5
     for ($j = 1; $j <= 5; $j++) {
-        
         $resultat = $i * $j;
-        
-        // La largeur totale est définie à 5 caractères
         $str = str_pad($resultat, 5);
-        
         echo $str;
     }
-    
     echo "\n";
 }
 ?>
