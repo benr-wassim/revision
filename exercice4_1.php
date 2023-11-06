29a2a15d8a12b1543e45a05533f4b5033783d83e
benr-wassim <benboubaker-wassim@lycee-jeanrostand.fr>
2023-11-06 18:01:51 +0100

    exercice 4.1

--- exercice4_1.php
+++ exercice4_1.php
@@ -1,6 +1,19 @@
 <?php
 function tri_selection($tab){
-    $n =
+    $n = sizeof($tab);
+    for($i = 0; $i< $n - 2 ; $i++){
+        $min = $i;
+        for($j = $i+1; $j < $n-1 ; $j++){
+            if($tab[$j]< $t[$min]){
+                $min = $j;
+            }
+        }
+        if ($min != $i){
+            $reste = $tab[$i];
+            $tab[$i] = $tab[$min];
+            $tab[$min] = $reste;
+        }
+    }
 }
 
 ?>
